c8aee27ccb3f3b0fa009a5aece85cc8bccd30bee
zahrablend <zahra@LAPTOP-010BK67M>
2022-11-11 16:09:48 +0200

    Tasks for second project part defined as comments

--- floorCalcMethods/Program.cs
+++ floorCalcMethods/Program.cs
@@ -4,9 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            //Task 1: Make the entire program a method so that you can execute it
+            //in your Main() method with one line: CalculateTotalCost();
+
+            //Task 2: Determine the total cost for the Taj Mahal in Agra, India
+            //Square 90.5m x 90.5 m (minus 4 equal-seded trianges of (24x24 m).
+            //Trangle area = 1/2 * L * H
+
+            //Task 3: Determine the total cost for the Al-Masjid al-haram
+            //(Great Mosque) in Mecca, Saudi Arabia
+            //Square 180m x 106 m + (square 284m X 264m - (square 84m x 264m) /2)
+
+            //Task 4: Allow user to pick which building result to display
+            // Use inquiry message: Al-Masjid al-haram (Great Mosque) in Mecca, Saudi Arabia
+            //Give user options with buiding names;
+            //Display result as such: "The plan for {building} in {place} costs {totallCost}!"
         }
 
-       
+
     }
 }
